3f3dd22b430ee725dc3238045708af06b4848dae
PedroJoaquin <pedro_25@live.com>
2015-10-03 23:51:19 -0600

    Update VectorHeap_implementa.java

--- src/VectorHeap_implementa.java
+++ src/VectorHeap_implementa.java
@@ -1,16 +1,48 @@
+/**Universidad Del Valle de Guatemala 
+ *Algoritmos y Estructura de Datos 
+ *Seccion 10- Hoja de Trabajo 8
+ *------------------------------------------------------------------
+ *@author
+ *Pedro Joaquin Castillo 14224
+ *Julio Ronaldo Dieguez 14475
+ *------------------------------------------------------------------
+ *VectorHeap_implementa
+ **/
 
 import java.util.Vector;
 
+/**
+ * @author PedroJoaquin
+ *
+ * @param <E>
+ */
+/**
+ * @author PedroJoaquin
+ *
+ * @param <E>
+ */
+/**
+ * @author PedroJoaquin
+ *
+ * @param <E>
+ */
 public class VectorHeap_implementa<E extends Comparable<E>> implements PriorityQueue<E>{
 
 	protected Vector<E> data; // the data, kept in heap order
 	
+	/**
+	 * Constructor de la clase. Crea un nuevo VectorHeap
+	 */
 	public VectorHeap_implementa()
 	// post: constructs a new priority queue
 	{
 		data = new Vector<E>();
 	}
 	
+	/**
+	 * Constructor de la clase. Convierte un vector en un VectorHeap 
+	 * @param v Vector que se desea convertir a VectorHeap 
+	 */
 	public VectorHeap_implementa(Vector<E> v)
 	// post: constructs a new priority queue from an unordered vector
 	{
@@ -22,6 +54,11 @@ public class VectorHeap_implementa<E extends Comparable<E>> implements PriorityQ
 			}
 	}
 	
+	/**
+	 * Retorna el padre del nodo indicado 
+	 * @param i Nodo del cual se desea conocer el padre 
+	 * @return Padre del Nodo en la posicion i 
+	 */
 	protected static int parent(int i)
 	// pre: 0 <= i < size
 	// post: returns parent of node at location i
@@ -29,6 +66,11 @@ public class VectorHeap_implementa<E extends Comparable<E>> implements PriorityQ
 		return (i-1)/2;
 	}
 	
+	/**
+	 * Retorna el hijo izquierdo del nodo indicado 
+	 * @param i Nodo del cual se desea conocer el hijo 
+	 * @return Localidad del hijo izquierdo del nodo 
+	 */
 	protected static int left(int i)
 	// pre: 0 <= i < size
 	// post: returns index of left child of node at location i
@@ -36,6 +78,12 @@ public class VectorHeap_implementa<E extends Comparable<E>> implements PriorityQ
 		return 2*i+1;
 	}
 	
+	
+	/**
+	 * Retorna el hijo derecho del nodo indicado 
+	 * @param i	Nodo del cual se desea conocer el hijo derecho
+	 * @return Localidad del hijo derecho del nodo 
+	 */
 	protected static int right(int i)
 	// pre: 0 <= i < size
 	// post: returns index of right child of node at location i
@@ -43,6 +91,10 @@ public class VectorHeap_implementa<E extends Comparable<E>> implements PriorityQ
 		return 2*(i+1);
 	}
 
+	/**
+	 * Mueve el nodo indicado a una posicion apropiada 
+	 * @param leaf Nodo que debe ser movido 
+	 */
 	protected void percolateUp(int leaf)
 	// pre: 0 <= leaf < size
 	// post: moves node at index leaf up to appropriate position
@@ -58,6 +110,10 @@ public class VectorHeap_implementa<E extends Comparable<E>> implements PriorityQ
 		data.set(leaf,value);
 	}
 	
+	/**
+	 * Mueve el nodo localizado en la raiz a una posicion apropiada dentro del subarbol
+	 * @param root Nodo que se desea mover 
+	 */
 	protected void pushDownRoot(int root)
 	// pre: 0 <= root < size
 	// post: moves node at index root down
@@ -91,6 +147,9 @@ public class VectorHeap_implementa<E extends Comparable<E>> implements PriorityQ
 	}
 
 	
+	/* (non-Javadoc)
+	 * @see HDT8.src.PriorityQueue#add(java.lang.Comparable)
+	 */
 	public void add(E value)
 	// pre: value is non-null comparable
 	// post: value is added to priority queue
@@ -99,6 +158,9 @@ public class VectorHeap_implementa<E extends Comparable<E>> implements PriorityQ
 		percolateUp(data.size()-1);
 	}
 	
+	/* (non-Javadoc)
+	 * @see HDT8.src.PriorityQueue#remove()
+	 */
 	@Override
 	public E remove() {
 		//pre: !isEmpty()
@@ -111,24 +173,36 @@ public class VectorHeap_implementa<E extends Comparable<E>> implements PriorityQ
 	}
 
 
+	/* (non-Javadoc)
+	 * @see HDT8.src.PriorityQueue#isEmpty()
+	 */
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/* (non-Javadoc)
+	 * @see HDT8.src.PriorityQueue#size()
+	 */
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return data.size();
 	}
 
+	/* (non-Javadoc)
+	 * @see HDT8.src.PriorityQueue#clear()
+	 */
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see HDT8.src.PriorityQueue#getFirst()
+	 */
 	@Override
 	public E getFirst() {
 		// TODO Auto-generated method stub
